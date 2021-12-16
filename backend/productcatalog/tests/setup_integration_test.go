75a6c5de9b5ba4c6e99b0eb8ae46c6584a52de64
Bartłomiej Klimczak <bartlomiej.klimczak88@gmail.com>
2021-12-16 18:08:54 +0100

    feat: integration tests in CI/CD

--- backend/productcatalog/tests/setup_integration_test.go
+++ backend/productcatalog/tests/setup_integration_test.go
@@ -4,13 +4,31 @@ package tests
 
 import "fmt"
 import "github.com/bkielbasa/go-ecommerce/backend/productcatalog/adapter"
+import "os"
 
 func init() {
-	conn := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable", "localhost", 5432, "bartlomiejklimczak", "ecommerce")
+	pass := getEnv("POSTGRES_PASSWORD", "")
+	var conn string
+
+	if pass != "" {
+		conn = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", getEnv("POSTGRES_HOST", "localhost"), getEnv("POSTGRES_PORT", "5432"), getEnv("POSTGRES_USER", "bartlomiejklimczak"), pass, getEnv("POSTGRES_DB", "ecommerce"))
+	} else {
+		conn = fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable", getEnv("POSTGRES_HOST", "localhost"), getEnv("POSTGRES_PORT", "5432"), getEnv("POSTGRES_USER", "bartlomiejklimczak"), getEnv("POSTGRES_DB", "ecommerce"))
+	}
+
 	s, err := adapter.NewPostgres(conn)
 	if err != nil {
 		panic("cannot establish connection to postgres: " + err.Error())
 	}
 
 	storage = s
+}
+
+func getEnv(name, def string) string {
+	v := os.Getenv(name)
+	if v == "" {
+		v = def
+	}
+
+	return v
 }
