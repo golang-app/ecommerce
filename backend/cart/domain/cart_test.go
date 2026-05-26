53a1eaf148393ae122898c9be426ce939d092e27
Bartłomiej Klimczak <bartlomiej.klimczak@invia.com>
2026-05-26 22:13:32 +0200

    Reapply "feat(cart): reject cross-currency arithmetic"
    This reverts commit 65dd00d649702041438bc4d2bdf556a62e2b26d3.

--- backend/cart/domain/cart_test.go
+++ backend/cart/domain/cart_test.go
@@ -1,6 +1,7 @@
 package domain_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/bkielbasa/go-ecommerce/backend/cart/domain"
@@ -9,6 +10,7 @@ import (
 
 var pID = domain.NewProduct("productID", "test product", 200, domain.MustNewCurrency("PLN"))
 var pID2 = domain.NewProduct("productID2", "test product", 1000, domain.MustNewCurrency("PLN"))
+var pUSD = domain.NewProduct("productUSD", "usd product", 500, domain.MustNewCurrency("USD"))
 
 func TestCart_Adding_Products_To_Cart_Should_Change_Quantity(t *testing.T) {
 	is := is.New(t)
@@ -48,4 +50,32 @@ func TestCart_CalculateTotalPrice(t *testing.T) {
 
 	// then
 	is.Equal(4, c.TotalQuantity())
+
+	// total = 1 * 200 + 3 * 1000 = 3200 PLN-minor
+	total := c.TotalPrice()
+	is.Equal(int64(3200), total.Amount())
+	is.Equal(domain.MustNewCurrency("PLN"), total.Currency())
+}
+
+func TestCart_RejectsCrossCurrencyItem(t *testing.T) {
+	is := is.New(t)
+	// given a cart with a PLN item
+	c := domain.NewCart(domain.NewUser(""))
+	is.NoErr(c.Add(pID, 1))
+
+	// when adding a USD item
+	err := c.Add(pUSD, 1)
+
+	// then it must fail with ErrCurrencyMismatch and not mutate the cart
+	is.True(errors.Is(err, domain.ErrCurrencyMismatch))
+	is.Equal(1, c.TotalQuantity())
+}
+
+func TestCart_EmptyTotalPrice(t *testing.T) {
+	is := is.New(t)
+	c := domain.NewCart(domain.NewUser(""))
+
+	total := c.TotalPrice()
+	is.Equal(int64(0), total.Amount())
+	is.Equal(domain.Currency(""), total.Currency())
 }
